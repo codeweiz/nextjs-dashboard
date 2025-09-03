ddfeb472ed1d2453a79dd4a63684fea3e048e443
codeweiz <13955645241@163.com>
2025-09-03 19:16:39 +0800

    feat(try catch): try catch 机制保证程序不崩溃/卡死，redirect 逻辑应在 try catch 后，否则不会执行（因为重定向的原理是抛出一个 Error，放在 try catch 中会被捕获导致不生效）

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -19,6 +19,7 @@ const CreateInvoice = FormSchema.omit({id: true, date: true});
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 
+// 新建发票
 export async function createInvoice(formData: FormData) {
     const {customerId, amount, status} = CreateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -28,15 +29,22 @@ export async function createInvoice(formData: FormData) {
 
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        console.error(error);
+    }
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 
+// 更新发票
 export async function updateInvoice(id: string, formData: FormData) {
     const {customerId, amount, status} = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -46,21 +54,32 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId},
-            amount      = ${amountInCents},
-            status      = ${status}
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId},
+                amount      = ${amountInCents},
+                status      = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        console.error(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
+
+// 删除发票
 export async function deleteInvoice(id: string) {
-    await sql`DELETE
-              FROM invoices
-              WHERE id = ${id}`;
+    try {
+        await sql`DELETE
+                  FROM invoices
+                  WHERE id = ${id}`;
+    } catch (error) {
+        console.error(error);
+    }
+
     revalidatePath('/dashboard/invoices');
 }
